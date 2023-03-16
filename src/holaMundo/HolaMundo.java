63eabd71c8768c6abb373fbc189eccb126fa2afe
David Gómez Vicente <David Gmez Vicente@DESKTOP-P6AM0N1>
2023-03-16 01:08:10 +0100

    Aqui hemos realizado los cambios

---
+++ src/holaMundo/HolaMundo.java
@@ -0,0 +1,15 @@
+package holaMundo;
+
+public class HolaMundo {
+	public static void main(String[] args) {
+		System.out.println("Hola Amigos");
+		
+		llamarNombre("David");
+		
+	}
+	
+	private static void llamarNombre(String nombre) {
+		System.out.println("Soy" + nombre );
+
+	}
+}
